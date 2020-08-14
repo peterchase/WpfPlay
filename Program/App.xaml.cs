fcada060df82c579b7c84ec44ee7cc7e77b1538e
Peter Chase <pete@peterchase.me>
2020-08-14 14:32:07 +0100

    Use a sensible namespace

--- Program/App.xaml.cs
+++ Program/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Program
+namespace FridayAfternoon.WpfPlay.Program
 {
     /// <summary>
     /// Interaction logic for App.xaml
