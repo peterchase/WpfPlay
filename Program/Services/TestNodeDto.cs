0233b7f800113a310badfbdf6d67629cbaf40b05
Peter Chase <pete@peterchase.me>
2020-08-15 17:19:45 +0100

    Add DTO

---
+++ Program/Services/TestNodeDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FridayAfternoon.WpfPlay.Program.Services
+{
+    public sealed class TestNodeDto : IEquatable<TestNodeDto>
+    {
+        public TestNodeDto(int id, string name, int parentId)
+        {
+            Id = id;
+            Name = name;
+            ParentId = parentId;
+        }
+
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public int ParentId { get; }
+
+        public override bool Equals(object obj) => obj is TestNodeDto dto && Equals(dto);
+
+        public bool Equals(TestNodeDto other) => Id == other.Id && Name == other.Name && ParentId == other.ParentId;
+
+        public override int GetHashCode() => Id;
+    }
+}
---
+++ Program/Services/TestNodeService.cs
@@ -0,0 +1,63 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DynamicData;
+
+namespace FridayAfternoon.WpfPlay.Program.Services
+{
+    public sealed class TestNodeService
+    {
+        private readonly SourceCache<TestNodeDto, int> mTestNodes = new SourceCache<TestNodeDto, int>(x => x.Id);
+
+        public static TestNodeService CreateWithRandomContent()
+        {
+            var service = new TestNodeService();
+            service.mTestNodes.AddOrUpdate(CreateTestNodes(25000));
+            return service;
+        }
+
+        private TestNodeService() { }
+
+        public IObservableCache<TestNodeDto, int> TestNodes => mTestNodes.AsObservableCache();
+
+        public void Move(TestNodeDto dto, int newParentId)
+        {
+            // TODO go to service then update the cache
+
+            //update the cache 
+            mTestNodes.AddOrUpdate(new TestNodeDto(dto.Id, dto.Name, newParentId));
+        }
+
+        public void Remove(TestNodeDto dto)
+        {
+            // TODO go to service then updated the cache
+
+            mTestNodes.Edit(updater =>
+            {
+                //assign new parent to the children of the removed test node
+                var childrenToMove = updater.Items
+                                    .Where(tn => tn.ParentId == dto.Id)
+                                    .Select(dto => new TestNodeDto(dto.Id, dto.Name, dto.ParentId))
+                                    .ToArray();
+
+                updater.AddOrUpdate(childrenToMove);
+
+                //get rid of the existing test node
+                updater.Remove(dto.Id);
+            });
+        }
+
+        private static IEnumerable<TestNodeDto> CreateTestNodes(int numberToLoad)
+        {
+            var random = new Random();
+
+            return Enumerable.Range(1, numberToLoad)
+                .Select(i =>
+                {
+                    var boss = i % 1000 == 0 ? 0 : random.Next(0, i);
+                    return new TestNodeDto(i, $"Person {i}", boss);
+                });
+        }
+    }
+}
