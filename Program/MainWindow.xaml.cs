e40e4d4b9cdd80a8d1a3a275bce49b71b82f9cb9
Peter Chase <pete@peterchase.me>
2020-08-22 10:58:17 +0100

    First attempt at xaml

--- Program/MainWindow.xaml.cs
+++ Program/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using FridayAfternoon.WpfPlay.Program.ViewModels;
 
 namespace FridayAfternoon.WpfPlay.Program
 {
@@ -20,9 +8,12 @@ namespace FridayAfternoon.WpfPlay.Program
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MainWindowViewModel mViewModel = new MainWindowViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = mViewModel;
         }
     }
 }
